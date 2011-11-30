893ca238f374ba2a3dac6ff9732157dc28648301
Macario <macarui@gmail.com>
2011-11-30 01:04:22 -0600

    first commit

---
+++ self_inspect.rb
@@ -0,0 +1,17 @@
+                                   class Proc
+                                     def in_discomfort?; :me; end
+                                   end
+you_are = you = 
+ 
+ ->(you) do
+  self.inspect until true
+  until nil
+    break you
+                                   end
+  puts you.in_discomfort?
+  you_are[you]
+                                   end
+
+you[
+        you_are
+]
